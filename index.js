3defb294d04213a5e87023a7b551b1c03f3fcecb
Stella Vilar <vilar.stella@gmail.com>
2020-05-30 15:06:11 +0200

    Views and ejs installed: integration is ok

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const PORT  = process.env.PORT || 1234 ;
 const express = require ('express');
 const app = express();
 
+//Import fichiers statiques
+app.use(express.static('public'));
+
+//Import views from ejs
+app.set('view engine', 'ejs');
+app.set('views', 'app/views');
+
+
+const router = require ('./app/router');
+app.use(router);
+
 //lancement de l'application
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
