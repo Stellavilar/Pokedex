8f2497a198d11e0e1d79594c16827bf032f4079b
Stella Vilar <vilar.stella@gmail.com>
2020-05-30 14:28:40 +0200

    MainController installed

---
+++ app/controllers/mainController.js
@@ -0,0 +1,17 @@
+const db_connection = require ('../db_connection');
+
+const mainController = {
+    homePage: (req,res) => {
+        db_connection.getAllPokemons((err, data) => {
+            if (err) {
+                console.log(err);
+                return res.send(err);
+            }
+            res.render('home', {
+                pokemons: data.rows
+            });
+        });
+    },
+};
+
+module.exports = mainController;
